16fa35d1929931cd32f236b09bd0765aa4793b26
mylearningjourneyalt <mylearningjourneyalt@outlook.com>
2024-09-12 15:44:31 -0400

    How to add FormControls Into a FormGroup, update html to save submit form on enter or click, access forms submitted values

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,36 +1,40 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
-  styleUrls: ['./edit-contact.component.css']
+  styleUrls: ['./edit-contact.component.css'],
 })
 export class EditContactComponent implements OnInit {
-  private contactsService = inject(ContactsService)
-  firstName = new FormControl('');
-  lastName = new FormControl();
-  dateOfBirth = new FormControl();
-  favoritesRanking = new FormControl();
+  private contactsService = inject(ContactsService);
+  contactForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(),
+    dateOfBirth: new FormControl(),
+    favoritesRanking: new FormControl(),
+  });
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
-    if (!contactId) return
+    if (!contactId) return;
 
-    this.contactsService.getContact(contactId).subscribe(contact => {
-      if(!contact) return
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if (!contact) return;
 
-      this.firstName.setValue(contact.firstName)
-      this.lastName.setValue(contact.lastName)
-      this.dateOfBirth.setValue(contact.dateOfBirth)
-      this.favoritesRanking.setValue(contact.favoritesRanking)
-    })
+      this.contactForm.controls.firstName.setValue(contact.firstName);
+      this.contactForm.controls.lastName.setValue(contact.lastName);
+      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
+      this.contactForm.controls.favoritesRanking.setValue(contact.favoritesRanking);
+    });
   }
 
+
   saveContact() {
-    console.log(this.firstName.value)
+    console.log(this.contactForm.value);
+    console.log(this.contactForm.controls.firstName.value);
   }
 }
