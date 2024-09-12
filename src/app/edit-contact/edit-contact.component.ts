85c33410f72136c0e3607dbeb95ba02df798f2d2
mylearningjourneyalt <mylearningjourneyalt@outlook.com>
2024-09-12 15:41:58 -0400

    How to add a FormControl to an Input, add initial values from subscription, retrieve value after click

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
+  private contactsService = inject(ContactsService)
+  firstName = new FormControl('');
+  lastName = new FormControl();
+  dateOfBirth = new FormControl();
+  favoritesRanking = new FormControl();
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+
+    this.contactsService.getContact(contactId).subscribe(contact => {
+      if(!contact) return
+
+      this.firstName.setValue(contact.firstName)
+      this.lastName.setValue(contact.lastName)
+      this.dateOfBirth.setValue(contact.dateOfBirth)
+      this.favoritesRanking.setValue(contact.favoritesRanking)
+    })
   }
 
   saveContact() {
-
+    console.log(this.firstName.value)
   }
 }
