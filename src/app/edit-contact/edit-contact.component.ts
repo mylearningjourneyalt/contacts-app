1c419dc1cbeb2479ba5e0f8aa0849f0ebec0aef2
mylearningjourneyalt <mylearningjourneyalt@outlook.com>
2024-09-12 16:20:41 -0400

    Now that formGroup matches interface, we can set FormGroup formcontrols to suscribed response directly

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -15,8 +15,8 @@ export class EditContactComponent implements OnInit {
     id: '',
     firstName: '',
     lastName: '',
-    dateOfBirth: <Date | null> null,
-    favoritesRanking: <number | null> null,
+    dateOfBirth: <Date | null>null,
+    favoritesRanking: <number | null>null,
 
     phone: this.formBuilder.nonNullable.group({
       phoneNumber: '',
@@ -40,35 +40,8 @@ export class EditContactComponent implements OnInit {
 
     this.contactsService.getContact(contactId).subscribe((contact) => {
       if (!contact) return;
-      this.contactForm.controls.id.setValue(contact.id);
-      this.contactForm.controls.firstName.setValue(contact.firstName);
-      this.contactForm.controls.lastName.setValue(contact.lastName);
-      this.contactForm.controls.dateOfBirth.setValue(contact.dateOfBirth);
-      this.contactForm.controls.favoritesRanking.setValue(
-        contact.favoritesRanking
-      );
 
-      this.contactForm.controls.phone.controls.phoneNumber.setValue(
-        contact.phone.phoneNumber
-      );
-      this.contactForm.controls.phone.controls.phoneType.setValue(
-        contact.phone.phoneType
-      );
-      this.contactForm.controls.address.controls.streetAddress.setValue(
-        contact.address.streetAddress
-      );
-      this.contactForm.controls.address.controls.city.setValue(
-        contact.address.city
-      );
-      this.contactForm.controls.address.controls.state.setValue(
-        contact.address.state
-      );
-      this.contactForm.controls.address.controls.postalCode.setValue(
-        contact.address.postalCode
-      );
-      this.contactForm.controls.address.controls.addressType.setValue(
-        contact.address.addressType
-      );
+      this.contactForm.setValue(contact);
     });
   }
 
