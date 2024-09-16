cc2ac36b918e7e30786db09d23e4feaaffcd8434
mylearningjourneyalt <mylearningjourneyalt@outlook.com>
2024-09-16 16:33:40 -0400

    Register directive with angular a a ng value accessor

--- src/app/date-value-accessor/date-value-accessor.directive.ts
+++ src/app/date-value-accessor/date-value-accessor.directive.ts
@@ -1,9 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef, Provider } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
+
+//allows angular to use this where appropriate
+const DATE_VALUE_PROVIDER: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => DateValueAccessorDirective),
+  multi: true
+}
 
 //target all input elements of type date
 //also only if they have a form control name directive, formControl directive, or ngModel Directive
 @Directive({
   selector: 'input([type=date])[formControlName],input([type=date])[formControl],input([type=date])[ngModel]',
+  providers: [DATE_VALUE_PROVIDER]
 })
 export class DateValueAccessorDirective {
 
